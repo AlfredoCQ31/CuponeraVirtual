c6329809f722419e7851bcbf161a85b0a6d41c19
AlfredoCQ31 <accahuanticoq@gmail.com>
2025-02-04 09:24:31 -0500

    all change 3

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,27 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
   plugins: [vue()],
-  // resolve: {
-  //   alias: {
-  //     "@": "/src",
-  //   },
-  // },
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+  build: {
+    cssCodeSplit: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["vue"],
+          app: ["/src/main.js"],
+        },
+        entryFileNames: "assets/[name].js",
+        chunkFileNames: "assets/[name].js",
+        assetFileNames: "assets/[name].[ext]",
+      },
+    },
+  },
   server: {
     watch: {
       usePolling: true,
@@ -16,13 +29,3 @@ export default defineConfig({
   },
   publicDir: "public",
 });
-
-// export default defineConfig({
-//   plugins: [vue()],
-//   server: {
-//     watch: {
-//       usePolling: true,
-//     },
-//   },
-//   publicDir: 'public',
-// })
